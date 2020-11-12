f13a733955e9ab89427cbaf8923d4a7d5f7b7235
Stanley Masinde <stanmasinde@gmail.com>
2020-11-12 13:00:26 +0300

    Added eslint
    Changes
    	new file:   .eslintrc.json
    	modified:   dist/validator.js
    	modified:   jest.config.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/validator.ts
    	modified:   tests/validator.test.js
    	deleted:    validator.js

--- jest.config.js
+++ jest.config.js
@@ -4,9 +4,9 @@
  */
 
 module.exports = {
-  clearMocks: true,
-  coverageDirectory: "coverage",
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-  testEnvironment: "node",
-};
+	clearMocks: true,
+	coverageDirectory: 'coverage',
+	// Indicates which provider should be used to instrument code for coverage
+	coverageProvider: 'v8',
+	testEnvironment: 'node',
+}
--- src/validator.ts
+++ src/validator.ts
@@ -1,3 +1,8 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable no-mixed-spaces-and-tabs */
 class Validator {
     /**
      * The fields under validation
@@ -20,26 +25,26 @@ class Validator {
      * @param rules 
      */
     constructor(fields: {}, rules: {}) {
-        this.fields = fields
-        this.rules = rules
-        this.messages = {}
+    	this.fields = fields
+    	this.rules = rules
+    	this.messages = {}
     }
 
     /**
      * Run the validator
      */
     validate() {
-        return new Promise((resolve, reject) => {
-            Object.entries(this.rules).forEach(rule => {
-                // Create a message bag
-                this.messages[rule[0]] = [] // eg. {email: required|email} will create {email: []}
-                this.getFieldRules(rule[1], rule[0])
-            })
-            let errorLengths = Object.values(this.messages).map((el: []) => el.length)
-            errorLengths.reduce((pre: any, cur: any) => {
-                return pre + cur
-            }) === 0 ? resolve('valid') : reject({ errors: this.messages })
-        })
+    	return new Promise((resolve, reject) => {
+    		Object.entries(this.rules).forEach(rule => {
+    			// Create a message bag
+    			this.messages[rule[0]] = [] // eg. {email: required|email} will create {email: []}
+    			this.getFieldRules(rule[1], rule[0])
+    		})
+    		const errorLengths = Object.values(this.messages).map((el: []) => el.length)
+    		errorLengths.reduce((pre: any, cur: any) => {
+    			return pre + cur
+    		}) === 0 ? resolve('valid') : reject({ errors: this.messages })
+    	})
     }
 
     /**
@@ -48,8 +53,8 @@ class Validator {
      * @param field 
      */
     private getFieldRules(rules: any, field: string) {
-        const rulesArray = rules.split('|') // required|email will be ['required', email]
-        this.validateField(rulesArray, field)
+    	const rulesArray = rules.split('|') // required|email will be ['required', email]
+    	this.validateField(rulesArray, field)
     }
 
     /**
@@ -58,30 +63,30 @@ class Validator {
      * @param fields 
      */
     private validateField(rules: string[], field: string) {
-        rules.forEach(r => {
-            const rule = r.split(':') // If the rule has a second param eg min:0
-            const ruleParam = parseInt(rule[1])
+    	rules.forEach(r => {
+    		const rule = r.split(':') // If the rule has a second param eg min:0
+    		const ruleParam = parseInt(rule[1])
 
-            switch (rule[0]) {
-                case 'required':
-                    this.required(field)
-                    break;
-                case 'email':
-                    this.email(field)
-                    break
-                case 'string':
-                    this.string(field)
-                    break
-                case 'min':
-                    this.min(field, ruleParam)
-                    break
-                case 'max':
-                    this.max(field, ruleParam)
-                    break;
-                default:
-                    break;
-            }
-        })
+    		switch (rule[0]) {
+    		case 'required':
+    			this.required(field)
+    			break
+    		case 'email':
+    			this.email(field)
+    			break
+    		case 'string':
+    			this.string(field)
+    			break
+    		case 'min':
+    			this.min(field, ruleParam)
+    			break
+    		case 'max':
+    			this.max(field, ruleParam)
+    			break
+    		default:
+    			break
+    		}
+    	})
     }
 
     /**
@@ -89,27 +94,26 @@ class Validator {
      * @param {string} field 
      */
     private friendlyName(field: string) {
-        return `The ${field.split('_').join(' ')}`
+    	return `The ${field.split('_').join(' ')}`
     }
     /**
      * The filed under validation mus not be null
      * @param {String} field 
      */
     private required(field: string) {
-        return this.fields[field] == '' || this.fields[field] == null ? this.messages[field].push(`${this.friendlyName(field)} is required`) : true
+    	return this.fields[field] == '' || this.fields[field] == null ? this.messages[field].push(`${this.friendlyName(field)} is required`) : true
     }
     /**
      * Validate an email address
      * @param {String} field 
      */
     private email(field: string) {
-        let rgx = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
-        if (rgx.test(this.fields[field]) == true) {
-
-        }
-        else {
-            this.messages[field].push(`${this.friendlyName(field)} should be a valid E-mail`)
-        }
+    	const rgx = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+    	if (rgx.test(this.fields[field]) == true) {
+    		return
+    	} else {
+    		this.messages[field].push(`${this.friendlyName(field)} should be a valid E-mail`)
+    	}
     }
 
     /**
@@ -117,8 +121,8 @@ class Validator {
      * @param {String} field 
      */
     private string(field: string) {
-        let rgx = new RegExp(/[A-Za-z]/)
-        return rgx.test(this.fields[field]) ? true : this.messages[field].push(`${this.friendlyName(field)} should be a string`)
+    	const rgx = new RegExp(/[A-Za-z]/)
+    	return rgx.test(this.fields[field]) ? true : this.messages[field].push(`${this.friendlyName(field)} should be a string`)
     }
 
     /**
@@ -127,10 +131,10 @@ class Validator {
      * @param {Number} limit 
      */
     private min(field: string, limit: number) {
-        if (this.fields[field] == undefined) {
-            return
-        }
-        return this.fields[field].length < limit ? this.messages[field].push(`${this.friendlyName(field)}  should be longer than ${limit}`) : true
+    	if (this.fields[field] == undefined) {
+    		return
+    	}
+    	return this.fields[field].length < limit ? this.messages[field].push(`${this.friendlyName(field)}  should be longer than ${limit}`) : true
     }
 
     /**
@@ -139,10 +143,10 @@ class Validator {
      * @param {Number} limit 
      */
     private max(field: string, limit: number) {
-        if (this.fields[field] == undefined) {
-            return
-        }
-        return this.fields[field].length > limit ? this.messages[field].push(`${this.friendlyName(field)} should be shorter than ${limit}`) : true
+    	if (this.fields[field] == undefined) {
+    		return
+    	}
+    	return this.fields[field].length > limit ? this.messages[field].push(`${this.friendlyName(field)} should be shorter than ${limit}`) : true
     }
 }
 
